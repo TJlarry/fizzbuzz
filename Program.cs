4a56650e9734e91d7c1549ae0d02f16127bd8303
TJlarry <alliokelanre@gmail.com>
2021-10-04 13:11:54 -0500

    fizz buzz

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the numbers of players and press Enter:");
+              int m =int.Parse( Console.ReadLine());
+
+            for (int  n = 1; n <= m; n++) {
+
+             
+                if(n%3== 0)
+                {
+                    Console.Write("Fizz,");
+                }
+
+                else if (n%5==0)
+                {
+                    Console.Write("Buzz,");
+
+                }
+                else if (n%3 == 0 && n%5==0)
+                {
+
+                    Console.Write("FizzBuzz,");
+                }
+                else 
+                {
+
+                    Console.Write($"{n},");
+                }
+
+               
+            }           
+        }
+    }
+}
